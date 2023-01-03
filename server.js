4e337f67fac17325482e7c85493a538b5ebb7334
Semion <rsemion@gmail.com>
2023-01-03 22:06:20 +0200

    commit

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ let express = require('express');
 let port = process.env.PORT || 3000;
 let app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/login.html');
+	res.sendFile(__dirname + '/views/login.html');
 });
 
 app.listen(port, () => {
 	console.log('App listening on port %d!', port);
+});
+
+app.get('/index.html', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
 });
